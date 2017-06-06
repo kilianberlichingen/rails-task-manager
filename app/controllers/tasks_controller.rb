553973038514502fc44379438cb30a8984bcb529
kilianberlichingen <kilian39@hotmail.de>
2017-06-06 16:23:21 +0200

    exercise 1 done

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,54 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show,:edit,:update,:destroy]
+
+def index
+  @tasks = Task.all
+end
+
+def show
+end
+
+def new
+  @task = Task.new
+end
+
+def create
+  t = Task.new(task_params)
+  t.save
+  redirect_to tasks_path
+end
+
+
+def edit
+
+end
+
+def update        # PATCH /tasks/:id
+  @task.update(task_params)
+  redirect_to tasks_path
+end
+
+def destroy       # DELETE /tasks/:id
+  @task.destroy
+  redirect_to tasks_path
+end
+
+private
+
+
+def set_task
+  @task = Task.find(params[:id])
+end
+
+def task_params
+  params.require(:task).permit(:name)
+end
+
+
+end
+
+
+
+
+
+
